097e04683be94edf27375f5991db927ae18d47e3
RohithKP <rohithkp.me@gmail.com>
2016-03-10 13:52:29 +0530

    added dashboard with basic layout

--- app/controllers/tabsDemoCtrl.js
+++ app/controllers/tabsDemoCtrl.js
@@ -1,6 +1,8 @@
 angular.module('QCrowd').controller('TabsDemoCtrl', function ($scope, $window) {
-  $scope.projects =  [{"id":0,"name":"Marius Lorem","status":"passed","browsers":["opera"],"user":"Alex Paul"},
-                      {"id":1,"name":"Dk Marius Lorem","status":"passed","browsers":["opera","mozilla"],"user":"Maria Ann"}];
+  $scope.projects =  [{"id":0,"name":"Marius Lorem","status":{"text":"passed","percent":50},"browsers":[{"name":"firefox","status":1},{"name":"chrome","status":0},{"name":"safari","status":-1},{"name":"opera","status":1}],"user":{"id":1,"imgUrl":"assets/images/users/one.jpg","name":"Alex Paul"}},
+                      {"id":1,"name":"Dk Marius Lorem","status":{"text":"passed","percent":75},"browsers":[{"name":"firefox","status":1},{"name":"chrome","status":1},{"name":"safari","status":0},{"name":"opera","status":1}],"user":{"id":2,"imgUrl":"assets/images/users/two.jpg","name":"Maria Ann"}},
+                    {"id":2,"name":"Dk Marius Lorem","status":{"text":"failed","percent":75},"browsers":[{"name":"firefox","status":-1},{"name":"chrome","status":0},{"name":"safari","status":0},{"name":"opera","status":0}],"user":{"id":3,"imgUrl":"assets/images/users/two.jpg","name":"Julie Ann"}},
+                  {"id":3,"name":"Dk Marius Lorem","status":{"text":"passed","percent":100},"browsers":[{"name":"firefox","status":-1},{"name":"chrome","status":1},{"name":"safari","status":1},{"name":"opera","status":1}],"user":{"id":4,"imgUrl":"assets/images/users/two.jpg","name":"Akshaya"}}];
   $scope.tabs = [
     { title:'TASK UNDER PROGRESS', content:'' },
     { title:'COMPLETED TASK', content:'Dynamic content 2'},
